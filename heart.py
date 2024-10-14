9f2ca47d0ca09fa872abfca881b6faf393591156
rip-on <shipon.ripon123@gmail.com>
2024-10-14 12:35:56 +0600

    add heart

---
+++ heart.py
@@ -0,0 +1,18 @@
+from turtle import *
+# from tkinter import *
+#
+# window = Tk()
+# window.geometry("700x400")
+# window.title("Heart 💝")
+# icon = PhotoImage(file = 'love.png')
+# window.iconphoto(True,icon)
+color('red')
+begin_fill()
+left(50)
+forward(100)
+circle(40,180)
+left(260)
+circle(40,180)
+forward(100)
+end_fill()
+done()
